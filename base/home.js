0f01b0b77cb3f163dfc12d10bb7c833d30de82fb
gelleicsaba <gellei.csaba@gmail.com>
2023-04-21 15:58:06 +0200

    -

---
+++ base/home.js
@@ -0,0 +1,26 @@
+class Home extends SFComponent {
+
+    state = {
+        /* specify the state vars here */
+    }
+
+    title = "Page title";
+
+    template = 
+`:Div
+    Your template content
+`;
+
+    constructor(obj) { super(obj); }
+
+    render() {
+        super.render();
+    }
+
+    /* evaluate your variables
+     *    before render      */
+    evaluate() {
+        super.evaluate();
+    }
+
+}
